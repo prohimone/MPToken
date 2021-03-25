95389aa03d2ba3dfca69ef79839f9a1b6a3fd93b
prohimone <prohimone@gmail.com>
2021-03-25 21:17:56 +0700

    add approve, transfer from, and allowance

--- MPToken.sol
+++ MPToken.sol
@@ -1,48 +1,53 @@
 pragma solidity ^0.8.3;
 
-contract MPDollarv2{
-    address public owner;
+contract MPToken{
     mapping(address => uint) public balances;
+    mapping(address => mapping(address => uint)) public allowance;
     uint public totalSupply = 100000000*10**18;
-    string public name = "Owner";
-    string public symbol = "Owner";
+    string public name = "MP Token";
+    string public symbol = "MP";
     uint public decimals = 18;
     
     event Transfer(address indexed from, address indexed to, uint value);
-    
+    event Approval(address indexed owner, address indexed spender, uint value);
+
+
     constructor(){
         balances[msg.sender] = totalSupply;
-        owner = msg.sender;
     }
     
-     function getOwner() public returns (address) {
-        return owner;
-     }
-     
-     function getTotalSupply() public returns (uint){
-         return totalSupply;
-     }
-     
     function balanceOf(address owner) public view returns (uint){
         return balances[owner];
     }
     
-    
     function transfer(address to, uint value) public returns(bool){
-        require(owner == msg.sender);
         require(balanceOf(msg.sender)>= value, 'balance too low');
         balances[to] += value; 
         balances[msg.sender] -= value;
         emit Transfer(msg.sender, to, value);
         return true;
     }
-    
+
+    function transferFrom(address from, address to, uint value) public returns(bool){
+        require(balanceOf(from)>= value, 'balance too low');
+        require(allowance[from][msg.sender] >= value, 'allowance too low');
+        balances[to] += value;
+        balances[from] -= value;
+        emit Transfer(from, to, value);
+        return true;
+    }
+
+    function approve(address spender, uint value) public returns(bool){
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
     function transferToAllHolders() public{
-        require(owner == msg.sender);
         require(balanceOf(msg.sender)>= 0, 'balance too low');
+
         //transfer to all shareholders (hardcoded) the amount they have
         transfer(0x8F1d055256f6C93121eAb173638DC5e13dddf829, 100000*10**18);
         emit Transfer(msg.sender, 0x8F1d055256f6C93121eAb173638DC5e13dddf829, 100000*10**18);
-
     }
 }
