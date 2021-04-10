931ec16bd5a0f88d6382d6a3fa36b8d47245f4c6
prohimone <prohimone@gmail.com>
2021-04-10 10:25:13 +0700

    added owner

--- contracts/MPToken.sol
+++ contracts/MPToken.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract MPToken is ERC20{
+    address public owner;
 
     constructor(string memory name, string memory symbol, uint256 initialSupply) public ERC20(name, symbol) {
+        owner = msg.sender;
         _mint(msg.sender, initialSupply);
     }
 }
